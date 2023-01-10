8241f6ce9b95baf18e5821dcaad4a3d21a96b99b
denius <dan.thegame78@gmail.com>
2023-01-10 17:48:20 +0200

    Створення таблиць через міграцію

---
+++ yiiProject/migrations/m230110_153259_create_topic_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%topic}}`.
+ */
+class m230110_153259_create_topic_table extends Migration
+
+{
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function safeUp()
+
+    {
+
+        $this->createTable('{{%topic}}', [
+
+            'id' => $this->primaryKey(),
+
+            'name' => $this->string(),
+
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function safeDown()
+
+    {
+
+        $this->dropTable('{{%topic}}');
+
+    }
+
+}
+?>
