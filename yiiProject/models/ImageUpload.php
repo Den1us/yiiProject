21146ef248046ffe5eb920c9d271d0fc5b7b9dfd
denius <dan.thegame78@gmail.com>
2023-01-17 03:13:47 +0200

    Картинка завантажується, назва шифрується, до 1.90

--- yiiProject/models/ImageUpload.php
+++ yiiProject/models/ImageUpload.php
@@ -11,17 +11,26 @@ class ImageUpload extends Model
 
     public $image;
 
-    public function rules(){
-        return[
-            [['image'], 'required'],
-            [['image'],'file', 'extensions' => 'jpg,png']
-        ];
-    }
-
-
-    public function uploadFile(UploadedFile $file)
+//    public function rules(){
+//        return[
+//            [['image'], 'required'],
+//            [['image'],'file', 'extensions' => 'jpg,png']
+//        ];
+//    }
+//
+//
+    public  function uploadFile(UploadedFile $file, $currentImage)
     {
-        $file->saveAs(Yii::getAlias('@web') . 'uploads/' . $file->name);
+        $this->image = $file;
+
+        unlink(Yii::getAlias('@web') . 'uploads/' . $currentImage);
+
+        $filename = strtolower(md5(uniqid($file->baseName)) . '.' . $file->extension);
+
+        $file->saveAs(Yii::getAlias('@web') . 'uploads/' . $filename);
+
+        return $filename;
+
     }
 
 //    public function deleteCurrentImage($currentImage)
--- yiiProject/modules/admin/controllers/ArticleController.php
+++ yiiProject/modules/admin/controllers/ArticleController.php
@@ -140,15 +140,12 @@ class ArticleController extends Controller
         $model = new ImageUpload;
 
         if(Yii::$app->request->isPost){
-
             $article = $this->findModel($id);
-
             $file = UploadedFile::getInstance($model,'image');
-            $model ->UploadFile($file);
-
-            if($article->saveImage($model->uploadFile($file, $article->image))){
-                return $this->redirect(['view', 'id'=>$article->id]);
-            }
+            $article->saveImage($model->uploadFile($file, $article->image));
+//            if($article->saveImage($model->uploadFile($file, $article->image))){
+//                return $this->redirect(['view', 'id'=>$article->id]);
+//            }
         }
 
         return $this->render('image',['model'=>$model]);
